8551f601e4d5b994b129662e0fcb45d745c28145
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2024-12-23 19:38:16 +0100

    Initial

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import estraverse from 'estraverse';
+import esprima from 'esprima-next';
+import escodegen from 'escodegen';
+import postcss from 'postcss';
+// @ts-ignore
+import postcssMinify from 'postcss-minify';
+
+import type {
+    Program,
+    Node,
+    TaggedTemplateExpression,
+    Identifier,
+} from 'estree';
+
+const postCssProcessor = postcss(postcssMinify());
+
+export default function(source: string) {
+    const parsedProgram= esprima.parseScript(source) as Program;
+
+    estraverse.replace(parsedProgram, {
+        enter(node: Node, parent: Node | null) {
+            if (node.type === "TaggedTemplateExpression") {
+                const taggedNode = node as TaggedTemplateExpression;
+
+                if (taggedNode.tag.type !== "Identifier")
+                    return;
+
+                const tagIdentifier = taggedNode.tag as Identifier;
+
+                switch (tagIdentifier.name) {
+                case "css":
+                    node.quasi.quasis = node.quasi.quasis.map(q => {
+                        q.value = {raw: postCssProcessor.process(q.value.raw).css.trim()};
+                        return q;
+                    });
+                    break;
+                case "html":
+                    node.quasi.quasis = node.quasi.quasis.map(q => {
+                        q.value = {
+                            raw: q.value.raw.replace(/(\n\s+|\s+\n)/gm, '')
+                        };
+                        return q;
+                    });
+                }
+            }
+
+            return node;
+        }
+    });
+
+    return {contents: escodegen.generate(parsedProgram)};
+}
+
